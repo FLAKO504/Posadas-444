ed25bd511bab4906c938cad44a976fc1d725a694
FLAKO504 <onanposadas7@gmail.com>
2025-04-10 12:33:05 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -300,7 +300,9 @@ conn: this,
                     fail('private', m, this)
                     continue
                 }
-                
+                if (plugin.register == true && _user.registered == false) { 
+                    fail('unreg', m, this)
+                    continue
                 }
                 m.isCommand = true
                 let xp = 'exp' in plugin ? parseInt(plugin.exp) : 17 
