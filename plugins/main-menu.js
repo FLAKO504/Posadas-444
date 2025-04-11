792b01cb006fd43a29b38cde46b45eb27ac68cd0
FLAKO504 <onanposadas7@gmail.com>
2025-04-11 14:11:11 -0600

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -168,8 +168,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
    }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
-    let pp = 'https://i.ibb.co/CPVcnqH/file.jpg'
-    let pp2 = 'https://i.ibb.co/9WrytGt/file.jpg'
+    let pp = 'https://i.postimg.cc/hv94Q6R8/IMG-20250408-WA0103.jpg'
+    let pp2 = 'https://i.postimg.cc/hv94Q6R8/IMG-20250408-WA0103.jpg'
     let pp3 = 'https://i.ibb.co/CPVcnqH/file.jpg'
     let pp4 = 'https://i.ibb.co/9WrytGt/file.jpg'
     let pp5 = 'https://i.ibb.co/CPVcnqH/file.jpg'
@@ -183,7 +183,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let pp13 = 'https://i.ibb.co/Cs6Tt9V/Sylph.jpg'
     let pp14 = 'https://i.ibb.co/JmcS3kv/Sylph.jpg'
     let pp15 = 'https://i.ibb.co/Cs6Tt9V/Sylph.jpg'
-    let img = 'https://qu.ax/Mvhfa.jpg'
+    let img = 'https://i.postimg.cc/hv94Q6R8/IMG-20250408-WA0103.jpg'
     let img2 = 'https://d.uguu.se/iqqLBUfF.jpg'
     await m.react('⭐')
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
