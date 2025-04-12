984d516c3ff336285973bc0ac82192d33c99c13a
FLAKO504 <onanposadas7@gmail.com>
2025-04-11 20:20:27 -0600

    Actualizar group-precios1.js

--- plugins/group-precios1.js
+++ plugins/group-precios1.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
 
     await m.react('💫');
 
-    const message = `AQUI ESTAN LOS PRECIOS.\n\n> 1 semana de spaam = 1k de diamantes\n> 5 mes = 800 diamantes\n> 3 días = 500 diamantes `;
+    const message = `AQUI ESTAN LOS PRECIOS.\n\n> 1 semana de spam = 1k de diamantes\n> 5 mes = 800 diamantes\n> 3 días = 500 diamantes `;
 
     if (m.isGroup) {
         const imageUrl = 'https://i.postimg.cc/hv94Q6R8/IMG-20250408-WA0103.jpg';
