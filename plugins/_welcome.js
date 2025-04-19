dfbb30ac78abd6e5e78a3580713e7c4633a0013d
FLAKO504 <onanposadas7@gmail.com>
2025-04-18 22:34:12 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -9,7 +9,7 @@ export async function before(m, {conn, participants, groupMetadata}) {
 
     // Procesar la imagen para redimensionarla
   const resizedImg = await Jimp.read(imgBuffer);
-  resizedImg.resize(650, 350); // Cambia el tamaño a 500x500 píxeles (ajusta según tus necesidades)
+  resizedImg.resize(750, 400); // Cambia el tamaño a 500x500 píxeles (ajusta según tus necesidades)
   const img = await resizedImg.getBufferAsync(Jimp.MIME_JPEG);
 
   if (chat.bienvenida && m.messageStubType == 27) {
