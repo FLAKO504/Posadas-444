3e839e418b872227539a706e609733271eacd476
FLAKO504 <onanposadas7@gmail.com>
2025-04-19 12:16:25 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -7,7 +7,7 @@ export async function before(m, {conn, participants, groupMetadata}) {
   let img = await (await fetch(`${pp}`)).buffer()
   
 const resizedImg = await Jimp.read(imgBuffer);
-  resizedImg.resize(650, 350); // Cambia el tamaño a 500x500 píxeles (ajusta según tus necesidades)
+  resizedImg.resize(650, 350); 
   const img = await resizedImg.getBufferAsync(Jimp.MIME_JPEG);
 
   let chat = global.db.data.chats[m.chat];
